f085ad7ecc32022a2dea1332c9a23a2492442414
Felipe Castillo <1006071586@u.icesi.edu.co>
2023-06-26 22:18:36 -0500

    Home correction

--- homepage.js
+++ homepage.js
@@ -1,39 +1,21 @@
-//Ejecutar función en el evento click
-document.getElementById("btn_open").addEventListener("click", open_close_menu);
+document.getElementById('add-expression').addEventListener('click', function() {
+    var expression = document.createElement('div');
+    expression.className = 'expression';
+    expression.innerHTML = `
+        <select class="column">
+            <option value="">Seleccione una columna...</option>
+            <!-- Aquí irían las opciones de columnas -->
+        </select>
+        <select class="operator">
+            <option value="">Seleccione un operador...</option>
+            <!-- Aquí irían las opciones de operadores -->
+        </select>
+        <input class="value" type="text" placeholder="Valor">
+    `;
+    document.getElementById('rule-form').insertBefore(expression, this);
+});
 
-//Declaramos variables
-var side_menu = document.getElementById("menu_side");
-var btn_open = document.getElementById("btn_open");
-var body = document.getElementById("body");
-
-//Evento para mostrar y ocultar menú
-    function open_close_menu(){
-        body.classList.toggle("body_move");
-        side_menu.classList.toggle("menu__side_move");
-    }
-
-//Si el ancho de la página es menor a 760px, ocultará el menú al recargar la página
-
-if (window.innerWidth < 760){
-
-    body.classList.add("body_move");
-    side_menu.classList.add("menu__side_move");
-}
-
-//Haciendo el menú responsive(adaptable)
-
-window.addEventListener("resize", function(){
-
-    if (window.innerWidth > 760){
-
-        body.classList.remove("body_move");
-        side_menu.classList.remove("menu__side_move");
-    }
-
-    if (window.innerWidth < 760){
-
-        body.classList.add("body_move");
-        side_menu.classList.add("menu__side_move");
-    }
-
+document.getElementById('rule-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Aquí iría el código para crear la regla a partir de las expresiones
 });
