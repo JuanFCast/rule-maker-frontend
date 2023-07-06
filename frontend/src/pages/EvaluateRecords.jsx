0af4c88cac0f4fbd278f7633991a4c15c5428a96
Luis Miguel Ossa Arias <luismiguelossaarias05@gmail.com>
2023-07-05 20:02:45 -0500

    feature(evaulateRecords): the backend's request has been initialized.

--- frontend/src/pages/EvaluateRecords.jsx
+++ frontend/src/pages/EvaluateRecords.jsx
@@ -9,67 +9,104 @@ const EvaluateRecords = () => {
   const [result, setResult] = useState(null);
 
   useEffect(() => {
-    // Se tiene que reemplazar con los endpoints de backend
-    axios.get('http://localhost:8080/getRecords').then((response) => {
-      setData(response.data);
-    });
+    const baseUrl = "http://localhost:8080";
+    let response = "";
+    try {
+      axios.get(
+        baseUrl + "/table/get",
+        {
+          params: {
+            groupId: "MyGroup",
+            tableId: 1
+          },
+          headers: {
+            "Access-Control-Allow-Origin": baseUrl,
+            "MediaType": "application/json",
+            "Authorization": "Bearer " + localStorage.getItem('jwt')
+          }
+        }
+      ).then((response) => {
+        setData(response.data);
+      });
 
-    axios.get('http://localhost:8080/getRules').then((response) => {
-      setRules(response.data);
-    });
+      axios.get(
+        baseUrl + "/rule/getAll",
+        {
+          headers: {
+            "MediaType": "application/json",
+            "Authorization": "Bearer " + localStorage.getItem('jwt')
+          }
+        }
+      ).then((response) => {
+        setRules(response.data);
+      });
+
+  // Aquí está tu nueva línea de código
+  response.data.data.forEach(objeto => { delete objeto._class; });
+
+  console.log(response.data.data);
+
+  if (response.data.data && response.data.data.length > 0) {
+    const form = {};
+    Object.keys(response.data.data[0]).forEach(key => form[key] = '');
+    this.setState({ data: response.data.data, form });
+  }
+} catch (error) {
+  console.error(error);
+}    
   }, []);
 
-  const handleDataSelection = (data) => {
-    setSelectedData(data);
-  }
+const handleDataSelection = (data) => {
+  setSelectedData(data);
+}
 
-  const handleRuleSelection = (index) => {
-    setSelectedRuleIndex(index);
-  }
+const handleRuleSelection = (index) => {
+  setSelectedRuleIndex(index);
+}
 
-  const applyRule = () => {
-    // Aquí es donde se necesita implementar la lógica para evaluar la regla seleccionada en los datos seleccionados
-    // La implementación exacta depende de cómo estén estructuradas las reglas y de qué tipo de lógica quieran permitir
-    const result = "Verdadero"; // Este es un valor de demostración
-    setResult(`Al evaluar el registro ${selectedData.ID} contra la regla ${selectedRuleIndex + 1}, el resultado es ${result}.`);
-  }
+const applyRule = () => {
+  // Aquí es donde se necesita implementar la lógica para evaluar la regla seleccionada en los datos seleccionados
+  // La implementación exacta depende de cómo estén estructuradas las reglas y de qué tipo de lógica quieran permitir
+  const result = "Verdadero"; // Este es un valor de demostración
+  setResult(`Al evaluar el registro ${selectedData.ID} contra la regla ${selectedRuleIndex + 1}, el resultado es ${result}.`);
+}
 
-  return (
-    <div>
-      <h1>Motor de reglas</h1>
+return (
+  <div>
+    <h1>Motor de reglas</h1>
 
-      <h2>Selecciona un registro</h2>
-      <table>
-        <thead>
-          <tr>
-            {data.length > 0 && Object.keys(data[0]).map((key, index) => (
-              <th key={index}>{key}</th>
+    <h2>Selecciona un registro</h2>
+    <table>
+      <thead>
+        <tr>
+          {data.length > 0 && Object.keys(data[0]).map((key, index) => (
+            <th key={index}>{key}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row, index) => (
+          <tr key={index} onClick={() => handleDataSelection(row)}>
+            {Object.values(row).map((value, index) => (
+              <td key={index}>{value}</td>
             ))}
           </tr>
-        </thead>
-        <tbody>
-          {data.map((row, index) => (
-            <tr key={index} onClick={() => handleDataSelection(row)}>
-              {Object.values(row).map((value, index) => (
-                <td key={index}>{value}</td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-
-      <h2>Selecciona una regla</h2>
-      <ul>
-        {rules.map((rule, index) => (
-          <li key={index} onClick={() => handleRuleSelection(index)}>{rule.name}</li>
         ))}
-      </ul>
+      </tbody>
+    </table>
 
-      <button onClick={applyRule}>Aplicar regla</button>
+    <h2>Selecciona una regla</h2>
+    <ul>
+      {rules.map((rule, index) => (
+        <li key={index} onClick={() => handleRuleSelection(index)}>{rule.name}</li>
+      ))}
+    </ul>
 
-      {result !== null && <p>{result}</p>}
-    </div>
-  );
+    <button onClick={applyRule}>Aplicar regla</button>
+
+    {result !== null && <p>{result}</p>}
+  </div>
+);
 }
 
 export default EvaluateRecords;
